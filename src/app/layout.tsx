01ff686a0ca4a60e5a56b4ebceb455d0cd74bdee
ImValerio <valerio.valletta@icloud.com>
2024-08-24 12:05:00 +0200

    add nan and footer in layout + add meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,46 @@
+"use client"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { useRouter } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "ShareTimeWithMe",
-  description: "The easiest way to share your schedule with your friends!",
-};
+// export const metadata: Metadata = {
+//   title: "ShareTimeWithMe",
+//   description: "The easiest way to share your schedule with your friends!",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter()
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <head>
+        <title>ShareTimeWithMe</title>
+        <meta name="description" content="The easiest way to share your schedule with your friends" />
+        <meta name="keywords" content="schedule,schedule time with friends, time management" />
+        <meta name="author" content="Valerio Valletta" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body className={inter.className}>
+
+        <nav className='w-full p-2 bg-gray-900 text-white text-center flex justify-start items-center'>
+
+
+          <div onClick={() => router.push("/")} className="flex items-center cursor-pointer">
+            <img className="max-w-10" src='logo_lg_blu.png' />
+            <h3 className="mx-2 text-xl">
+              ShareTimeWithMe
+            </h3>
+          </div>
+        </nav>
+        {children}
+        <footer className='w-full p-2 bg-gray-900 text-white text-center'>Made with ❤️ by <a href='https://www.valeriovalletta.it'>Valerio Valletta</a></footer>
+      </body>
+    </html >
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,12 +17,11 @@ const Page = () => {
     <main className="flex min-h-screen flex-col items-center justify-between w-full">
       <div className="z-10 w-full md:max-w-xl items-center justify-between font-mono m-5 md:m-20 text-sm lg:flex flex-col">
         <div className='w-100 flex justify-center items-center m-2'>
-          <img src='logo_lg_blu.png'></img>
+          <img src='logo_lg_blu.png' />
         </div>
         <CreateInstance />
 
       </div>
-      <footer className='w-full p-2 bg-gray-900 text-white text-center'>Made with ❤️ by <a href='https://www.valeriovalletta.it'>Valerio Valletta</a></footer>
     </main>
   )
 }
