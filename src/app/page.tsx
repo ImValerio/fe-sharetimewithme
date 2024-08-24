b0c34183d01d14fee4f50aa664ef6e7b3ab48e1a
ImValerio <valerio.valletta@icloud.com>
2024-08-24 12:05:40 +0200

    remove useless code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,6 @@ import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 const Page = () => {
-  const router = useRouter()
-  const [binaryWeeks, setBinaryWeeks] = useState(["0000000", "0000000"])
-  const setBinaryWeek = (binaryWeek: string, i: number) => {
-    const newBinaryWeeks = [...binaryWeeks];
-    newBinaryWeeks[i] = binaryWeek;
-    setBinaryWeeks(newBinaryWeeks);
-  }
-  const [username, setUsername] = useState("")
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between w-full">
       <div className="z-10 w-full md:max-w-xl items-center justify-between font-mono m-5 md:m-20 text-sm lg:flex flex-col">
